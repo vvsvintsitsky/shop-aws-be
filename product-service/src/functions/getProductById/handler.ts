ca2363cd09b578f5d05ec543ec0d5f5d6ba67486
Uladzislau_Svintsitski <uladzislau_svintsitski@epam.com>
2022-07-14 19:26:29 +0200

    feat: handle unknown errors

--- product-service/src/functions/getProductById/handler.ts
+++ product-service/src/functions/getProductById/handler.ts
@@ -1,23 +1,27 @@
-import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
-import { middyfy } from '@libs/lambda';
-import { EntityNotFoundError } from '@repositories/errors/EntityNotFoundError';
+import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
+import { formatJSONResponse } from "@libs/api-gateway";
+import { middyfy } from "@libs/lambda";
+import { EntityNotFoundError } from "@repositories/errors/EntityNotFoundError";
 
-import { getProductRepository }from '@repositories/product'
-import { ProductRepository } from '@repositories/product/types';
+import { getProductRepository } from "@repositories/product";
+import { ProductRepository } from "@repositories/product/types";
 
-import schema from './schema';
+import schema from "./schema";
 
 export function createHandler(getProductRepository: () => ProductRepository) {
-  const getProductById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const getProductById: ValidatedEventAPIGatewayProxyEvent<
+    typeof schema
+  > = async (event) => {
     try {
-      return formatJSONResponse(await getProductRepository().getById(event.pathParameters.id));
+      return formatJSONResponse(
+        await getProductRepository().getById(event.pathParameters.id)
+      );
     } catch (error) {
       if (error instanceof EntityNotFoundError) {
         return formatJSONResponse({ message: error.message }, 404);
       }
-  
-      throw error
+
+      return formatJSONResponse({ message: "Unknown error" }, 500);
     }
   };
 
