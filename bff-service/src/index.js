796d41709fa1afcb817834da04046396c78b70a0
Uladzislau_Svintsitski <uladzislau_svintsitski@epam.com>
2022-08-30 14:41:57 +0200

    fix: replate https with http in proxy requests

--- bff-service/src/index.js
+++ bff-service/src/index.js
@@ -4,28 +4,29 @@ const { URLSearchParams } = require("url");
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use("*", (req, res) => {
 	const [, recipient, ...restPath] = req.originalUrl.split("/");
 
 	const recipientUrl = process.env[recipient];
 
 	if (!recipientUrl) {
-		res.status(200);
-        res.write(`${recipientUrl} | ${recipient}`);
-        res.end();
+		res.sendStatus(502);
 		return;
 	}
 
 	fetch(
 		recipientUrl
+			.replace('https', 'http')
 			.concat("/")
 			.concat(restPath.join("/"))
 			.concat(new URLSearchParams(req.query)),
-		{ method: req.method, headers: req.headers, body: req.body, query }
+		{ method: req.method, body: req.body }
 	).then((actual) => {
 		res.status(actual.status);
 		actual.body.pipe(res);
 	});
 });
 
-app.listen(4000);
+app.listen(port);
